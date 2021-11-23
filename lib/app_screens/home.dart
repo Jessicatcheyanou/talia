d9b452f7dd96ef888a7a98af2ceda89a5baf1e06
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2021-11-23 02:51:57 +0100

    Jess | Column and Expanded Widgets | Rows and Columns

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,19 +6,87 @@ class Home extends StatelessWidget {
     // TODO: implement build
     return Center(
         child: Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurpleAccent,
-      margin: EdgeInsets.only(left: 35.0, top: 50.0),
-      child: Text(
-        "Flight",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 75.0,
-            fontFamily: "Raleway",
-            fontWeight: FontWeight.w700,
-            color: Colors.white),
-      ),
-    ));
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurpleAccent,
+            margin: EdgeInsets.only(left: 35.0, top: 50.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "Jessica",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 55.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Ines Kanmegne:A female software developer passionate of what she does",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Tcheyanou",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 55.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      "Jessica",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 55.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Ines Kanmegne:A female software developer passionate of what she does",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 35.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Tcheyanou",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 55.0,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                  ],
+                )
+              ],
+            )));
   }
 }
