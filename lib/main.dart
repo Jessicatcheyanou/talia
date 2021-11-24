8f3fe3bfa3b7faded5e0302968ed340b51f4b0b0
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2021-11-24 21:08:10 +0100

    Jess | Floating Action Button and SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,32 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: "Exploring UI Widgets",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Long List"),
-          ),
-          body: getListView())));
+        appBar: AppBar(
+          title: Text("Floating Action Button and Snack Bar"),
+        ),
+        body: getListView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB Clicked");
+          },
+          child: Icon(Icons.add),
+          tooltip: "Add one more Item",
+        ),
+      )));
+}
+
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text("You just tapped $item"),
+    action: SnackBarAction(
+      label: "UNDO",
+      onPressed: () {
+        debugPrint('Performing dummy UNDO Operation');
+      },
+    ),
+  );
+
+  Scaffold.of(context).showSnackBar(snackBar);
 }
 
 List<String> getListElements() {
@@ -23,7 +45,12 @@ Widget getListView() {
   var listItems = getListElements();
   var listView = ListView.builder(itemBuilder: (context, index) {
     return ListTile(
-        leading: Icon(Icons.import_contacts), title: Text(listItems[index]));
+      leading: Icon(Icons.import_contacts),
+      title: Text(listItems[index]),
+      onTap: () {
+        showSnackBar(context, listItems[index]);
+      },
+    );
   });
   return listView;
 }
