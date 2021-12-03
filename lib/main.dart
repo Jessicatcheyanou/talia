ded84e68b38387862a98e7c14d1465fb4926364a
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2021-12-03 19:07:18 +0100

    Jess | Simple Interest Calculator | Applying Styles and Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,6 +5,10 @@ void main() {
     debugShowCheckedModeBanner: false,
     title: "Stateful App Example",
     home: SIForm(),
+    theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent),
   ));
 }
 
@@ -24,6 +27,8 @@ class _SIFormState extends State<SIForm> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    TextStyle? textStyle = Theme.of(context).textTheme.subtitle1;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
@@ -38,9 +43,11 @@ class _SIFormState extends State<SIForm> {
                     top: _minimumPadding, bottom: _minimumPadding),
                 child: TextField(
                   keyboardType: TextInputType.number,
+                  style: textStyle,
                   decoration: InputDecoration(
                       labelText: 'Principal',
                       hintText: 'Enter Principal e.g. 12000',
+                      labelStyle: textStyle,
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(5.0))),
                 )),
@@ -49,9 +56,11 @@ class _SIFormState extends State<SIForm> {
                     top: _minimumPadding, bottom: _minimumPadding),
                 child: TextField(
                   keyboardType: TextInputType.number,
+                  style: textStyle,
                   decoration: InputDecoration(
                       labelText: 'Rate of Interest',
                       hintText: 'In percent',
+                      labelStyle: textStyle,
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(5.0))),
                 )),
@@ -63,9 +72,11 @@ class _SIFormState extends State<SIForm> {
                     Expanded(
                         child: TextField(
                       keyboardType: TextInputType.number,
+                      style: textStyle,
                       decoration: InputDecoration(
                           labelText: 'Term',
                           hintText: 'Time in years',
+                          labelStyle: textStyle,
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5.0))),
                     )),
@@ -89,19 +100,32 @@ class _SIFormState extends State<SIForm> {
               children: [
                 Expanded(
                     child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  textColor: Theme.of(context).primaryColorDark,
                   onPressed: () {},
-                  child: Text('Calculate'),
+                  child: Text(
+                    'Calculate',
+                    textScaleFactor: 1.5,
+                  ),
                 )),
                 Expanded(
                     child: RaisedButton(
+                  color: Theme.of(context).primaryColorDark,
+                  textColor: Theme.of(context).primaryColorLight,
                   onPressed: () {},
-                  child: Text('Reset'),
+                  child: Text(
+                    'Reset',
+                    textScaleFactor: 1.5,
+                  ),
                 ))
               ],
             ),
             Padding(
               padding: EdgeInsets.all(_minimumPadding * 2),
-              child: Text('Todo Text'),
+              child: Text(
+                'Todo Text',
+                style: textStyle,
+              ),
             )
           ],
         ),
@@ -113,8 +137,8 @@ class _SIFormState extends State<SIForm> {
     AssetImage assetImage = AssetImage('images/LDTV.jpg');
     Image image = Image(
       image: assetImage,
-      width: 125.0,
-      height: 125.0,
+      width: 150.0,
+      height: 150.0,
     );
 
     return Container(
